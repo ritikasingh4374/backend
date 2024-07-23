15e1c1ebb6de0a7540cf6a7bf0af33f6af9eb5b0
ritikasingh4374 <154665560+ritikasingh4374@users.noreply.github.com>
2024-07-23 15:55:46 +0530

    Add files via upload

---
+++ index4.js
@@ -0,0 +1,33 @@
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+var bandName ="";
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+function generateBandName (req, res, next){
+  console.log(req.body);
+  bandName = req.body["street"]+req.body["pet"];
+  next();
+}
+
+app.use(generateBandName);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html")
+});
+
+app.post("/submit" , (req, res) => {
+  res.send(`<h1>Your Band Name : </h1><h2>${bandName}</h2`)
+})
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
